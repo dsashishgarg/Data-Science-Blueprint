d1b141cafacd8105bea4adb12090c99104387da9
dsashishgarg <ashishgarg@live.in>
2022-02-21 22:07:48 +0530

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='sample_data_science_project',
+    author='Ashish Garg',
+    license='MIT',
+)
